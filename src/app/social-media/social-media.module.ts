872c5541030b06e17ede96c88cd9245a6da2f864
judev34 <julien.coduert34@gmail.com>
2022-08-21 19:17:48 +0200

    create card

--- src/app/social-media/social-media.module.ts
+++ src/app/social-media/social-media.module.ts
@@ -6,6 +6,7 @@ import { PostsService } from './services/posts.service';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { PostsResolver } from './resolvers/posts.resolver';
 import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 @NgModule({
@@ -15,7 +16,8 @@ import { PostListItemComponent } from './components/post-list-item/post-list-ite
   ],
   imports: [
     CommonModule,
-    SocialMediaRoutingModule
+    SocialMediaRoutingModule,
+    SharedModule
   ], 
   exports: [
     PostListComponent
