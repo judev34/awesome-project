b06ab0ac277e42877c45c13ae9b85209a8580cef
SolidJulien <31781936+judev34@users.noreply.github.com>
2022-08-23 13:21:29 +0200

    add doc list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { SocialMediaModule } from './social-media/social-media.module';
+import { DocumentListComponent } from './document/components/document-list/document-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DocumentListComponent
   ],
   imports: [
     BrowserModule,
