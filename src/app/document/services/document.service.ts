9d5ffe9eeb95dc1f9440fbbbed59ed239fb6ccfe
SolidJulien <31781936+judev34@users.noreply.github.com>
2022-08-23 13:30:30 +0200

    edit model / service

--- src/app/document/services/document.service.ts
+++ src/app/document/services/document.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { Document } from '../models/document.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DocumentService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getDocuments(): Observable<Document[]> {
+    return this.http.get<Document[]>(`${environment.apiUrlLaravel}/documents`)
+  }
 }
